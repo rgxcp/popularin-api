3c48a50fc185c564baa3a6e7fd0bc406c007a51c
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-06 18:14:25 +0700

    Memperbaiki migrasi

--- database/migrations/2020_04_03_075920_create_films_table.php
+++ database/migrations/2020_04_03_075920_create_films_table.php
@@ -17,7 +17,7 @@ class CreateFilmsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('tmdb_id')->unique();
             $table->string('title');
-            $table->year('year');
+            $table->date('release_date');
             $table->string('poster');
             $table->string('overview')->nullable();
             $table->bigInteger('contributor')->nullable();
