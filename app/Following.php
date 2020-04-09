1df9600538976ddccb7646d11f5b1488be7f94c2
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-09 19:44:47 +0700

    Mengisi model untuk following

--- app/Following.php
+++ app/Following.php
@@ -11,7 +11,25 @@ class Following extends Model
     ];
 
     protected $hidden = [
+        'user_id',
+        'following_id',
         'created_at',
         'updated_at'
     ];
+
+    public function following_info() {
+        return $this->belongsTo(User::class, 'following_id', 'id')->select([
+            'id',
+            'first_name',
+            'profile_picture'
+        ]);
+    }
+
+    public function follower_info() {
+        return $this->belongsTo(User::class, 'user_id', 'id')->select([
+            'id',
+            'first_name',
+            'profile_picture'
+        ]);
+    }
 }
