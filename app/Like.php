d2523d84566e7e3230223ba5a4a55a26f402b771
Rommy Gustiawan <rgcppp@gmail.com>
2020-04-20 17:48:02 +0700

    Membuat model untuk like

---
+++ app/Like.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $guarded = [
+        // All columns are guarded
+    ];
+
+    protected $hidden = [
+        'user_id',
+        'review_id',
+        'created_at',
+        'updated_at'
+    ];
+}
