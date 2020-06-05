d6799f29c7e2f5391ce70fb356daf8aa00d071fd
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-05 18:48:10 +0700

    Membuat model developer

---
+++ app/Developer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Developer extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable;
+    use Authorizable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        // All columns are guarded
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
