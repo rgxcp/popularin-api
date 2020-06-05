f8f7bef06b6b787f0c25ca853288946140b2b4b6
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-05 19:54:38 +0700

    Membuat middleware is developer

---
+++ app/Http/Middleware/IsDeveloper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Developer;
+
+class IsDeveloper
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $isDeveloper = Developer::where('api_token', $request->header('api_token'))->exists();
+
+        if (!$isDeveloper) {
+            return response()->json([
+                'status' => 616,
+                'message' => 'Invalid Credentials',
+                'problem' => 'You must sign up or sign in as developer to use all the feature of this API'
+            ]);
+        }
+        
+        return $next($request);
+    }
+}
