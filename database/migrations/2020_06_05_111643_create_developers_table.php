b896dfef48e4898112899ccf767f5e5a8e71737e
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-05 18:47:54 +0700

    Membuat migrasi developer

---
+++ database/migrations/2020_06_05_111643_create_developers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevelopersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('developers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('profile_picture');
+            $table->string('password');
+            $table->string('api_token', 100);
+            $table->string('token', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('developers');
+    }
+}
