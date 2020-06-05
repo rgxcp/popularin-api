dc56cc70a73ed5270315fb402d25c2366068bc3b
Rommy Gustiawan <rgcppp@gmail.com>
2020-06-05 18:49:27 +0700

    Menambahkan endpoint untuk controller developer

--- routes/web.php
+++ routes/web.php
@@ -12,36 +12,41 @@
 */
 
 $router->group(['prefix' => 'api'], function () use ($router) {
+    // Version
     $router->get('/', function () use ($router) {
         return $router->app->version();
     });
 
-    $router->get('status', 'DeveloperController@showStatus');
-
+    // Comment
     $router->get('review/{review_id}/comments', 'CommentController@shows');
     $router->post('comment', 'CommentController@create');
     $router->delete('comment/{id}', 'CommentController@delete');
 
+    // Following
     $router->get('film/{tmdb_id}/favorites/from/all', 'FavoriteController@showFilmFavoritesFromAll');
     $router->get('film/{tmdb_id}/favorites/from/following', 'FavoriteController@showFilmFavoritesFromFollowing');
     $router->get('user/{user_id}/favorites', 'FavoriteController@showUserFavorites');
     $router->post('film/{tmdb_id}/favorite', 'FavoriteController@create');
     $router->delete('film/{tmdb_id}/unfavorite', 'FavoriteController@delete');
 
+    // Film
     $router->get('film/{tmdb_id}/self', 'FilmController@self');
     $router->get('film/{tmdb_id}', 'FilmController@show');
 
+    // Following
     $router->get('user/{user_id}/followings', 'FollowingController@showFollowings');
     $router->get('user/{user_id}/followers', 'FollowingController@showFollowers');
     $router->get('user/{user_id}/mutuals', 'FollowingController@showMutuals');
     $router->post('user/{user_id}/follow', 'FollowingController@create');
     $router->delete('user/{user_id}/unfollow', 'FollowingController@delete');
 
+    // Like
     $router->get('review/{review_id}/likes/from/all', 'LikeController@showLikesFromAll');
     $router->get('review/{review_id}/likes/from/following', 'LikeController@showLikesFromFollowing');
     $router->post('review/{review_id}/like', 'LikeController@create');
     $router->delete('review/{review_id}/unlike', 'LikeController@delete');
 
+    // Review
     $router->get('film/{tmdb_id}/reviews/from/all', 'ReviewController@showFilmReviewsFromAll');
     $router->get('film/{tmdb_id}/reviews/from/following', 'ReviewController@showFilmReviewsFromFollowing');
     $router->get('film/{tmdb_id}/reviews/liked', 'ReviewController@showLikedReviews');
@@ -54,6 +59,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->put('review/{id}', 'ReviewController@update');
     $router->delete('review/{id}', 'ReviewController@delete');
 
+    // User
     $router->get('user/search/{query}', 'UserController@search');
     $router->get('user/self', 'UserController@self');
     $router->get('user/{id}', 'UserController@show');
@@ -62,11 +68,20 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('user/signout', 'UserController@signout');
     $router->put('user/{id}', 'UserController@update');
     $router->put('user/{id}/password', 'UserController@updatePassword');
-    // $router->delete('user/{id}', 'UserController@delete');
 
+    // Watchlist
     $router->get('film/{tmdb_id}/watchlists/from/all', 'WatchlistController@showFilmWatchlistsFromAll');
     $router->get('film/{tmdb_id}/watchlists/from/following', 'WatchlistController@showFilmWatchlistsFromFollowing');
     $router->get('user/{user_id}/watchlists', 'WatchlistController@showUserWatchlists');
     $router->post('film/{tmdb_id}/watchlist', 'WatchlistController@create');
     $router->delete('film/{tmdb_id}/unwatchlist', 'WatchlistController@delete');
+
+    // Developer
+    $router->get('developer/status', 'DeveloperController@showStatus');
+    $router->get('developer/self', 'DeveloperController@self');
+    $router->post('developer/signup', 'DeveloperController@signup');
+    $router->post('developer/signin', 'DeveloperController@signin');
+    $router->post('developer/signout', 'DeveloperController@signout');
+    $router->put('developer/{id}', 'DeveloperController@update');
+    $router->put('developer/{id}/password', 'DeveloperController@updatePassword');
 });
