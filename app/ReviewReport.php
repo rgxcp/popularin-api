a3741f7872aedee1b6e3989ecfb6b12d649bf308
Rommy Gustiawan <rgcppp@gmail.com>
2021-03-19 20:44:46 +0700

    Mengisi kerangka model -> fitur report

--- app/ReviewReport.php
+++ app/ReviewReport.php
@@ -6,4 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReviewReport extends Model
 {
+    protected $guarded = [
+        // All columns are guarded
+    ];
+
+    protected $hidden = [
+        'review_id',
+        'report_category_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $appends = [
+        'timestamp'
+    ];
+
+    public function reportCategory()
+    {
+        return $this->belongsTo(ReportCategory::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getTimestampAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
