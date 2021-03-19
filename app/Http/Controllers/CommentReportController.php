c4c83aafd5db815af87108a2b08d0d6340045e86
Rommy Gustiawan <rgcppp@gmail.com>
2021-03-19 20:45:09 +0700

    Mengisi kerangka controller -> fitur report

--- app/Http/Controllers/CommentReportController.php
+++ app/Http/Controllers/CommentReportController.php
@@ -2,6 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use App\CommentReport;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
 class CommentReportController extends Controller
 {
+    public function index($comment_id)
+    {
+        Carbon::setLocale('id');
+
+        $commentReports = CommentReport::with([
+            'reportCategory', 'user'
+        ])->where('comment_id', $comment_id)
+            ->orderBy('created_at', 'asc')
+            ->paginate(20);
+
+        return response()->json([
+            'status' => isset($commentReports[0]) ? 101 : 606,
+            'message' => isset($commentReports[0]) ? 'Request Retrieved' : 'Request Not Found',
+            'result' => isset($commentReports[0]) ? $commentReports : null
+        ]);
+    }
+
+    public function store(Request $request, $comment_id)
+    {
+        $comment = Comment::withCount('commentReports')->findOrFail($comment_id);
+
+        $authID = Auth::id();
+
+        if ($comment['user_id'] == $authID) {
+            return response()->json([
+                'status' => 686,
+                'message' => 'Can\'t Report Self'
+            ]);
+        } else {
+            CommentReport::firstOrCreate([
+                'user_id' => $authID,
+                'comment_id' => $comment_id,
+                'report_category_id' => $request['report_category_id']
+            ]);
+
+            if ($comment['comment_reports_count'] + 1 == env('NSFW_THRESHOLD')) {
+                $comment->delete();
+            }
+
+            return response()->json([
+                'status' => 202,
+                'message' => 'Request Created'
+            ]);
+        }
+    }
 }
--- app/Http/Controllers/ReviewReportController.php
+++ app/Http/Controllers/ReviewReportController.php
@@ -2,6 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Review;
+use App\ReviewReport;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
 class ReviewReportController extends Controller
 {
+    public function index($review_id)
+    {
+        Carbon::setLocale('id');
+
+        $reviewReports = ReviewReport::with([
+            'reportCategory', 'user'
+        ])->where('review_id', $review_id)
+            ->orderBy('created_at', 'asc')
+            ->paginate(20);
+
+        return response()->json([
+            'status' => isset($reviewReports[0]) ? 101 : 606,
+            'message' => isset($reviewReports[0]) ? 'Request Retrieved' : 'Request Not Found',
+            'result' => isset($reviewReports[0]) ? $reviewReports : null
+        ]);
+    }
+
+    public function store(Request $request, $review_id)
+    {
+        $review = Review::withCount('reviewReports')->findOrFail($review_id);
+
+        $authID = Auth::id();
+
+        if ($review['user_id'] == $authID) {
+            return response()->json([
+                'status' => 686,
+                'message' => 'Can\'t Report Self'
+            ]);
+        } else {
+            ReviewReport::firstOrCreate([
+                'user_id' => $authID,
+                'review_id' => $review_id,
+                'report_category_id' => $request['report_category_id']
+            ]);
+
+            if ($review['review_reports_count'] + 1 == env('NSFW_THRESHOLD')) {
+                $review->delete();
+            }
+
+            return response()->json([
+                'status' => 202,
+                'message' => 'Request Created'
+            ]);
+        }
+    }
 }
